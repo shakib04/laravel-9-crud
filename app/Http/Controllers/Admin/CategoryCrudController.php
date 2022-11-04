f42373c717c5cec7372ad436f7f57f6ea0f60d14
shakibul <shakibul.alam@bracits.com>
2022-11-04 20:18:35 +0600

    arrear master and item has added

--- app/Http/Controllers/Admin/CategoryCrudController.php
+++ app/Http/Controllers/Admin/CategoryCrudController.php
@@ -21,7 +21,7 @@ class CategoryCrudController extends CrudController
 
     /**
      * Configure the CrudPanel object. Apply settings to all operations.
-     * 
+     *
      * @return void
      */
     public function setup()
@@ -33,7 +33,7 @@ class CategoryCrudController extends CrudController
 
     /**
      * Define what happens when the List operation is loaded.
-     * 
+     *
      * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
      * @return void
      */
@@ -45,13 +45,13 @@ class CategoryCrudController extends CrudController
         /**
          * Columns can be defined using the fluent syntax or array syntax:
          * - CRUD::column('price')->type('number');
-         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']); 
+         * - CRUD::addColumn(['name' => 'price', 'type' => 'number']);
          */
     }
 
     /**
      * Define what happens when the Create operation is loaded.
-     * 
+     *
      * @see https://backpackforlaravel.com/docs/crud-operation-create
      * @return void
      */
@@ -59,19 +59,25 @@ class CategoryCrudController extends CrudController
     {
         CRUD::setValidation(CategoryRequest::class);
 
-        CRUD::field('name');
-        CRUD::field('slug');
+        //CRUD::field('name');
+        CRUD::field('slug')->value('test')->attributes(['readonly' => 'readonly']);
+        CRUD::addField([
+            'label' => 'name',
+            'name' => 'name',
+            'type' => 'text',
+            'readonly' => 'readonly',
+            ]);
 
         /**
          * Fields can be defined using the fluent syntax or array syntax:
          * - CRUD::field('price')->type('number');
-         * - CRUD::addField(['name' => 'price', 'type' => 'number'])); 
+         * - CRUD::addField(['name' => 'price', 'type' => 'number']));
          */
     }
 
     /**
      * Define what happens when the Update operation is loaded.
-     * 
+     *
      * @see https://backpackforlaravel.com/docs/crud-operation-update
      * @return void
      */
---
+++ database/migrations/2022_11_04_141224_create_arrears_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('arrears_items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('arrears_master_id')->constrained('arrears_masters');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('arrears_items');
+    }
+};
